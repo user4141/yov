b38322a8ad9ea3da1a263fb9910d8fd0e88a6c1a
user4141 <44729954+user4141@users.noreply.github.com>
2018-11-08 21:49:01 +0300

    Update wes.js

--- wes.js
+++ wes.js
@@ -1,17 +1,18 @@
 const Discord = require('discord.js');
-const A7MD = new Discord.Client();
+const client = new Discord.Client();
 
-console.log("BOT ONLINE");
- 
-A7MD.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم 
-مكان راقي ندعوك له :sparkles:
-                               [ https://discord.gg/Q87rKBE ] 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
-}).catch(console.error)
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.on('ready', async() => {
+var server = "510103671313268772"; // ايدي السررفر
+var channel = "510128835895885834";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا ')
+	},305);
 })
-A7MD.login(process.env.BOT_TOKEN);  //
+
+
+client.login("NTEwMTU5NzA5MTEwNjY1Mjk3.DsYWPA.U9q_UTGiyXrZ7lMqkMdv4Op6eM4");
